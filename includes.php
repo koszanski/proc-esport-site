7cd0abd5322f5605022c6dca0354b7100e2e2c5f
onoderaSecurity <48093884+onoderaSecurity@users.noreply.github.com>
2020-05-06 19:25:51 +0100

    Added an Includes and Config file

---
+++ includes.php
@@ -0,0 +1,3 @@
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"></link>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
--- shared/player.php
+++ shared/player.php
@@ -2,9 +2,7 @@
 <html>
 
 	<head>
-		<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"></link>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+		<?php include ("includes.php"); ?>
 	</head>
 
 
