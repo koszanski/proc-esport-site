ba337d1d3acc5b0ff2b617cb3a529b3cd1d64c45
onoderaSecurity <48093884+onoderaSecurity@users.noreply.github.com>
2020-05-07 20:23:00 +0100

    Login form created, Stylesheet modified

--- index.php
+++ index.php
@@ -5,12 +5,18 @@
 		<?php include ("includes.php"); ?>
 	</head>
 
-
 	<body>
+	<div class="login-container">
+		<form class="login-form">
+			<img src="images/logo.png" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="">
+			<input type="text" class="form-control" name="username" placeholder="Username" required="">
+			<input type="password" class="form-control" name="password" placeholder="Password" required="">
 
+			<a href="adminlogin.php">Admin Login</a>
+			<button class="btn btn-lg btn-primary btn-block">Login</button>
+			</div>
+		</form>
 
 	</body>
-
-
 
 </html>
