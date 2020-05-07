b065fe727ede58068018df8f0fc52ebb8c6a2e38
onoderaSecurity <48093884+onoderaSecurity@users.noreply.github.com>
2020-05-07 15:32:18 +0100

    Stylesheet added, added to includes

--- includes.php
+++ includes.php
@@ -1,4 +1,5 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"></link>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="stylesheet.css"></link>
 <script src ="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.4.0/cjs/popper-lite.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
