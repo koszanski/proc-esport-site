f3357b66f62033def4833a331869669f46c2a700
onoderaSecurity <48093884+onoderaSecurity@users.noreply.github.com>
2020-08-13 18:43:46 +0100

    updated bootstrap/popper/jquery

--- includes/includes.php
+++ includes/includes.php
@@ -1,5 +1,5 @@
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"></link>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"></link>
 <link rel="stylesheet" href="stylesheet.css"></link>
-<script src ="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.4.0/cjs/popper-lite.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src ="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.4.4/cjs/popper-lite.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
