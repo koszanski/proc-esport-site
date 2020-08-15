9b68212fecfa7bb78fb82d0b5dba9c2979257646
onoderaSecurity <48093884+onoderaSecurity@users.noreply.github.com>
2020-08-15 18:57:42 +0100

    initial login logic

--- includes/loginlogic.php
+++ includes/loginlogic.php
@@ -12,10 +12,50 @@ if (isset($_POST['login-submit'])) {
     }
     else {
         # code...
-        $sql = "SELECT ";
+        $sql = "SELECT * FROM user WHERE userLogin=?";
+        $stmt = mysqli_stmt_init($conn);
+
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../index.php?error=sqlerror");
+            exit();
+        }
+        else {
+        
+            mysqli_stmt_bind_param($stmt, "s", $user);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            if ($row = mysqli_fetch_assoc($result)) {
+                $passcheck = password_verify($pass, $row['userPass']);
+                if ($passcheck == true) {
+                    session_start();
+                    $_SESSION['activeLogin'] = $row['userLogin'];
+                    $_SESSION['activeID'] = $row['userID'];
+
+                    header("Location: ../index.php?login=successful");
+                    exit();
+                    
+                }
+                else if($passcheck == false)
+                {
+                    header("Location: ../index.php?error=wrongpass");
+                    exit();
+                }
+            }
+            else {
+                header("Location: ../index.php?error=usernotfound");
+                exit();
+            }
+
+       
+        }
 
     }
 
+}
+
+else {
+    header("Location: ../index.php");
+    exit();
 }
 
 
