8228e7530c4dd184359880596735d34abe044fc9
onoderaSecurity <48093884+onoderaSecurity@users.noreply.github.com>
2020-08-17 00:00:23 +0100

    reordered scripts

--- includes/includes.php
+++ includes/includes.php
@@ -1,6 +1,9 @@
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"></link>
 <link rel="stylesheet" href="stylesheet.css"></link>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src ="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.4.4/cjs/popper-lite.min.js"></script>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"></link>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 <script src="https://code.highcharts.com/highcharts.js"></script>
