022f024621e97013332591a2452934226093b787
onoderaSecurity <48093884+onoderaSecurity@users.noreply.github.com>
2020-08-18 00:54:25 +0100

    added objective logic op

---
+++ coach/coachlogic/addobjectivelogic.php
@@ -0,0 +1,40 @@
+<?php 
+if (isset($_POST['coach-objective-submit'])) {
+
+    require 'includes/dbconfig.php';
+
+    $goal = $_POST['objectiveGoal'];
+    $stattypeid = $_POST['StatTypeID'];
+    $deadline = $_POST['deadlineDate'];
+    $playerid = $_POST['selectedPlayer'];
+    $objective = "Active";
+
+    if (empty($goal) || empty($stattypeid) || empty($deadline) || empty($playerid)) {
+        header("Location: ../coachlanding.php?error=emptyfields");
+        exit();
+    }
+    else {
+            $sql = "INSERT INTO objective (objectiveGoal, objectiveStatTypeID, objectiveDeadline, objectivePlayerID, objectiveStatus) VALUES (?, ?, ?, ?, ?)";
+            $stmt = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_repare($stmt, $sql)) {
+                header("Location: ../coachlanding.php?error=sqlerror");
+                exit();
+            }
+            else {
+                mysqli_stmt_bind_param($stmt, "sssss", $goal, $stattypeid, $deadline, $playerid, $objective);
+                mysqli_stmt_execute($stmt);
+                header("Location: ../coachlanding.php?addobjective=complete");
+                exit();
+            }
+        }
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+}
+else {
+    header("Location: ../index.php");
+    exit();
+
+}
+
+?>
