eb729c453bda5ad79cd5394cf1eac1d7ff2deea1
koszanski <48093884+onoderaSecurity@users.noreply.github.com>
2020-08-23 15:35:43 +0100

    basic navigation added

--- player/playerlanding.php
+++ player/playerlanding.php
@@ -23,9 +23,37 @@
     			exit();
 			}
 		?>
+<!--view player, view objectives, view announcements, maybe view calendar, maybe view stat graph or implement into player page-->
+
+		<div class="row">
+		  <div class="col-xs-1-12">
+			<div class="card" style="width: 18rem;">
+  				<div class="card-body">
+  					<h5 class="card-title">View Profile</h5>
+    				<p class="card-text">View an overview of your profile.</p>
+    				<a href="../shared/player.php" class="btn btn-primary">Go</a>
+  				</div>
+			</div>
+		  </div>
+		  <div class="col-xs-1-12">
+		  	<div class="card" style="width: 18rem;">
+  				<div class="card-body">
+  					<h5 class="card-title">Objectives</h5>
+    				<p class="card-text">View your outstanding targets issued to you by your coach.</p>
+    				<a href="objectives.php" class="btn btn-primary">Go</a>
+  				</div>
+			</div>
+		  </div>
+		  <div class="col-xs-1-12">
+		  	<div class="card" style="width: 18rem;">
+  				<div class="card-body">
+  					<h5 class="card-title">Announcements</h5>
+    				<p class="card-text">View important issued announcements by your organisation.</p>
+    				<a href="announcements.php" class="btn btn-primary">Go</a>
+  				</div>
+			</div>
+		  </div>
+		</div>
 
 	</body>
-
-
-
 </html>
