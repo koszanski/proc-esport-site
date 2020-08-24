bce1b89c8cc90f807228e7a8dbd0e738f5144f37
koszanski <48093884+onoderaSecurity@users.noreply.github.com>
2020-08-25 00:20:11 +0100

    another placeholder

---
+++ shared/calendar.php
@@ -0,0 +1,22 @@
+<?php
+	require "../includes/header.php";
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include ("../includes/includes.php"); ?>
+	</head>
+
+
+	<body>
+
+		<?php
+			echo '<div class="alert alert-success" role="danger"> This has not been implemented, return to previous page.</div>';
+		?>
+
+	</body>
+
+
+
+</html>
