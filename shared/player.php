6ca9da926a3112fc08dd005b9bbff1dd1d2fb412
koszanski <48093884+onoderaSecurity@users.noreply.github.com>
2020-08-24 20:13:11 +0100

    player page now shows teamname/gamename

--- shared/player.php
+++ shared/player.php
@@ -4,7 +4,7 @@
     if (isset($_SESSION['activeCoachLogin'])) {
 		require '../includes/dbconfig.php';
 		$selecteduser = $_POST['usernames'];
-        $sql = "SELECT user.userID, user.userLogin, user.userName, team_players.teamID FROM user INNER JOIN team_players ON user.userID=team_players.teamPlayerID WHERE userLogin = '$selecteduser' ORDER BY userID DESC";
+        $sql = "SELECT user.userID, user.userLogin, user.userName, team.teamName, game.gameTitle FROM user INNER JOIN team_players ON user.userID=team_players.teamPlayerID INNER JOIN team ON team_players.teamID=team.teamID INNER JOIN game ON team.teamGameID=game.GameID WHERE userLogin = '$selecteduser' ORDER BY userID DESC";
 		$result = mysqli_query($conn, $sql);
     }
 ?>
@@ -26,10 +26,11 @@
 				{
 					if ($row = mysqli_fetch_assoc($result)) {	
 						echo " 	
-						<h3>ID ". $row['userID'] ."</h3>
-						<h3>Username ". $row['userLogin'] ."</h3>
-						<h3>Fullname ". $row['userName'] ."</h3>
-						<h3>Team ID". $row['teamID']. "</h3>
+						<h3>ID: ". $row['userID'] ."</h3>
+						<h3>Username: ". $row['userLogin'] ."</h3>
+						<h3>Fullname: ". $row['userName'] ."</h3>
+						<h3>Team Name: ". $row['teamName']. "</h3>
+						<h3>Game Name: ". $row['gameTitle']. "</h3>
 						";
 						exit();
 					}
@@ -47,10 +48,11 @@
 			{
 
 				echo " 	
-				<h3>ID ". $_SESSION['activePlayerID'] ."</h3>
-				<h3>Username ". $_SESSION['activePlayerLogin'] ."</h3>
-				<h3>Fullname ". $_SESSION['activePlayerFullname'] ."</h3>
-				<h3>Team ID". $_SESSION['activePlayerTeam']. "</h3>
+				<h3>ID: ". $_SESSION['activePlayerID'] ."</h3>
+				<h3>Username: ". $_SESSION['activePlayerLogin'] ."</h3>
+				<h3>Fullname: ". $_SESSION['activePlayerFullname'] ."</h3>
+				<h3>Team Name: ". $_SESSION['activePlayerTeamName']. "</h3>
+				<h3>Game Name: ". $_SESSION['activePlayerGameName']. "</h3>
 				";
 				//use session values to show you, the logged in user
 				exit();
@@ -61,7 +63,15 @@
     			exit();
             }
             //if player is logged in, just grab the player data from the session tag
-            //if coach is set, then use what was used from the post method on a seperate form
+			//if coach is set, then use what was used from the post method on a seperate form
+			
+
+			//concept for graph:
+			//game select dropdown, that pulls all games in db. has js onclick that makes an sql query every time a diff game is selected...
+			//gamemode select dropdown. ...gamemode query is changed to show the gamemodes only relevant to that game. this also has an onclick which then...
+			//stattype select dropdown. ...shows stattypes only relevant to the gamemode.
+			//submit button that shows the graph and makes these queries: 
+			//SELECT gamesessionstarttime, statvalue where playerid equals to selected player, AND stattypeid is equal to selected, order DESC gamesessionstarttime
 		?>
 
 	</body>
